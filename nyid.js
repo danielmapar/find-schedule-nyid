c9d6d04aa81e502b32ad9d221bfc5eaa1031791b
Daniel Parreira <danielmapar@daniels-mbp.mynetworksettings.com>
2022-11-22 02:14:52 -0500

    Fixing script

--- nyid.js
+++ nyid.js
@@ -7,18 +7,18 @@ async function findSchedule() {
         "accept": "application/json, text/plain, */*",
         "accept-language": "en-US,en;q=0.9",
         "ackrefid": "",
-        "browser_token": "N2Y0MmYwNTJiNDY2OGI3ZGM3NTkwZmM5ZjBjYzc3NDQ=",
+        "browser_token": "ZTVlNDhlOGU5OWJlMGY2ZTAxOTZlOWQxOWMxMDFiYjA=",
         "content-type": "application/json",
         "sec-ch-ua": "\"Google Chrome\";v=\"107\", \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"",
         "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-ch-ua-platform": "\"macOS\"",
         "sec-fetch-dest": "empty",
         "sec-fetch-mode": "cors",
         "sec-fetch-site": "same-origin",
-        "userid": "YmE3MjIyNDEtMmY5Ny00ZWY2LWI0NGQtZGRiMWM1NmZhOWEz",
+        "userid": "ZjQzYWQ4MzYtZjI3Zi00NzcxLWI3NTItNjNiMzZjYWI3MDE0",
         "x-idnyc-recaptchav3-token": "",
-        "x-idnyc-user-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImFkbWluIiwicm9sZSI6IlB1YmxpYyIsIm5iZiI6MTY2ODk4MjgwOSwiZXhwIjoxNjY4OTg2NDA5LCJpYXQiOjE2Njg5ODI4MDksImlzcyI6ImEwNjktaWRueWNvbmxpbmVwb3J0YWwubnljLmdvdiIsImF1ZCI6Imh0dHBzOi8vbXNwd3ZhLWhyYW1pcDEwLmNzYy5ueWNuZXQvSU9QV2ViU2VydmljZVNlY3VyaXR5LyJ9.6x5X9BH2CT1eODPxxhxHQsh6sD8_UZl4ecx42MZFI_o",
-        "cookie": "RT=\"z=1&dm=nyc.gov&si=m9kr6zicnep&ss=lapud6ea&sl=0&tt=0\"; _fbp=fb.1.1668977927583.1365732763; WT_FPC=id=8ab73160-db66-4fae-954b-d35a24b56698:lv=1668977929675:ss=1668977927429",
+        "x-idnyc-user-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImFkbWluIiwicm9sZSI6IlB1YmxpYyIsIm5iZiI6MTY2OTEwMDYzOSwiZXhwIjoxNjY5MTA0MjM5LCJpYXQiOjE2NjkxMDA2MzksImlzcyI6ImEwNjktaWRueWNvbmxpbmVwb3J0YWwubnljLmdvdiIsImF1ZCI6Imh0dHBzOi8vbXNwd3ZhLWhyYW1pcDEwLmNzYy5ueWNuZXQvSU9QV2ViU2VydmljZVNlY3VyaXR5LyJ9.oyPxftCjmjxdAGp3bjleHGrDVBqT2BvJgXZ_gvPyg9Y",
+        "cookie": "RT=\"z=1&dm=nyc.gov&si=cfpulrtuuio&ss=larv7nnv&sl=0&tt=0\"; WT_FPC=id=979903de-ac26-4720-8181-d77e1fad3bb8:lv=1669100633324:ss=1669100281802",
         "Referer": "https://a069-idnyconlineportal.nyc.gov/IOPWeb/",
         "Referrer-Policy": "strict-origin-when-cross-origin"
       }
@@ -33,8 +33,7 @@ async function findSchedule() {
         '3:20 PM','3:40 PM','4:00 PM','4:20 PM','4:40 PM','5:00 PM',
     ]
 
-    const numOfDays = 30
-    const promises = []
+    const numOfDays = 60
     for (let dayIndex = 0; dayIndex < numOfDays; dayIndex++) {
 
         date.setDate(date.getDate() + dayIndex);
@@ -46,15 +45,13 @@ async function findSchedule() {
         for (let startTime of startTimes) {
             requestBody["startTime"] = startTime
 
-            console.log(requestBody)
-
-            promises.push(fetch("https://a069-idnyconlineportal.nyc.gov/IOPWebServices/api/AppointmentApi/GetAvailableTimeSlots", {
+            const resp = await fetch("https://a069-idnyconlineportal.nyc.gov/IOPWebServices/api/AppointmentApi/GetAvailableTimeSlots", {
                 "headers": requestHeaders,
                 "body": JSON.stringify(requestBody),
                 "method": "POST"
-            }));
+            });
 
-            const data = await response.json();
+            const data = await resp.json();
 
             if (data.responseStatusDto && data.responseStatusDto.message && data.responseStatusDto.message == "Many Request From this Client") {
                 console.log("Too many requests to server, stopping script!")
@@ -64,11 +61,12 @@ async function findSchedule() {
             if (data.responseDataDto && data.responseDataDto.data && data.responseDataDto.data['$values'] && data.responseDataDto.data['$values'].length > 0) {
                 console.log("--------> Found a date for " + requestBody["startDate"] + " at " + requestBody["startTime"])
             } else {
-                console.log("Nothing found for " + requestBody["startDate"] + " at " + requestBody["startTime"])
+                // console.log("Nothing found for " + requestBody["startDate"] + " at " + requestBody["startTime"])
             }
+            
+            // Wait 5 seconds after checking a day for all times
+            await setTimeout(5000);
         }
-        // Wait 5 seconds after checking a day for all times
-        await setTimeout(5000);
     }
 }
 
